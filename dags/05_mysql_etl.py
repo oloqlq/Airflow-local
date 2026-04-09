37f070a0aa0500fc18f200160646404551ab80d3
oloqlq <oloqlqxown0919@gmail.com>
2026-04-09 14:18:22 +0900

    Feat: 모듈 가져오기, 기본 설정 추가

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -1,8 +1,32 @@
+'''
+- etl 간단하게 적용, 스마트팩토리상 온도 센서 데이터 처리 (mysql 사용)
+'''
+
+
 # 1. 모듈 가져오기
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import logging
+
+from airflow.providers.mysql.operators.mysql import MysqlOperator
+# Load 단계에서 SQL에 전처리된 데이터를 밀어넣을 때 사용
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+
+# 데이터
+import json
+import random
+import pandas as pd #소량의 데이터(규모 면에서)
+import os
+
+
+# 2. 환경변수
+# 프로젝트 내부 폴더를 데이터용으로 (~/dags/data) 지정
+# task 진행간 생성되는 파일을 동기화하도록 위치를 지정 -> 향후 s3로 대체될 수 있음
+
+# 도커 컨테이너의 워커 내부 airflow 상의 지정한 데이터 위치
+DATA_PATH = '/opt/airflow/dags/data' 
+os.mkdir(DATA_PATH, exist_ok=True)
 
 # 3-1. 콜백함수 정의
 
