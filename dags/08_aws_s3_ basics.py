7cc9b198edc6b88b1db04e28790a7dab22253c1d
oloqlq <oloqlqxown0919@gmail.com>
2026-04-14 17:09:06 +0900

    Feat: aws branch 시작/기본구성

--- dags/08_aws_s3_ basics.py
+++ dags/08_aws_s3_ basics.py
@@ -1,5 +1,11 @@
-
-# 1. 모듈 가져오기
+'''
+- 원격 PC에서 AWS S3에 데이터를 업로드하는 간단한 DAG
+    - 엑세스키가 잘 작동하는지 체크
+    - 데이터량에 다른 수행시간 체크 -> 데이터를  S3에 적제하는 방식에 대한 고민 (직접 or 서비스 이용)    
+- 설치 (호스트 PC, 로컬 PC상)
+    - pip install apache-airflow-providers-amazon
+'''
+# 1. 모듈가져오기
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -10,34 +16,19 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
 # 2. 환경변수 설정
-
-# 버킷 이름
-# 파일 이름
-# 파일 위치 
-
-BUCKET_NAME = "de-ai-14-827913617635-ap-northeast-1-an"
-FILE_NAME   = 'hello.txt'
-LOCAL_PATH  = f'opt/airflow/dags/data/{FILE_NAME}'
-
-
+BUCKET_NAME = "de-ai-14-827913617635-ap-northeast-1-an" 
+FILE_NAME   = 'hello.txt' 
+LOCAL_PATH  = f'/opt/airflow/dags/data/{FILE_NAME}'
 
 def _check_s3(**kwargs):
     hook = S3Hook(aws_conn_id='aws_default')
     keys = hook.list_keys(bucket_name=BUCKET_NAME)
-
     if not keys:
-        raise ValueError('업로드 실패')
+        raise ValueError('업로드 실패') 
     for key in keys:
         logging.info(f'키 : {key}')
 
-
-
-
     pass
-
-
-
-
 
 # 3. DAG 정의
 with DAG(
@@ -52,32 +43,25 @@ with DAG(
     start_date  = datetime(2026,2,25),     
     catchup     = False,
     tags        = ['aws', 's3'],
-) as dag:
+)as dag:
+    # 4. Task 정의 
     task_create_file = BashOperator(
         task_id = "create_file",
-        bash_command = f'echo "hello aiflow & s3" > {LOCAL_PATH}',
-
-
+        bash_command = f'echo "hello airflow & s3" > {LOCAL_PATH}'
     )
     task_upload_to_s3 = LocalFilesystemToS3Operator(
-        task_id     = "upload_to_s3",
-        filename    = LOCAL_PATH,
-        dest_key    = FILE_NAME,
-        dest_bucket = BUCKET_NAME,
-        aws_conn_id = 'aws_default',
-        replace     = True
+        task_id  = "upload_to_s3",
+        filename = LOCAL_PATH,  
+        dest_key = FILE_NAME,   
+        dest_bucket = BUCKET_NAME,  
+        aws_conn_id = 'aws_default', 
+        replace  = True      
     )
-    task_check_s3 = PythonOperator(
-        task_id         = "check_s3",
+    task_check_s3     = PythonOperator(
+        task_id = "check_s3",
         python_callable = _check_s3
-
     )
 
-
-
-    
-
-    # 의존성 정의
-    #task_create_file >> 
-    task_upload_to_s3 >> task_check_s3
+    # 5. 의존성
+    task_create_file >> task_upload_to_s3 >> task_check_s3
     pass
