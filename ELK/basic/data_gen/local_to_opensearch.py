6dfce677e328c243d0943a3eb30a7b795591bf3b
oloqlq <oloqlqxown0919@gmail.com>
2026-04-23 10:55:56 +0900

    Hotfix: .env수정

--- ELK/basic/data_gen/local_to_opensearch.py
+++ ELK/basic/data_gen/local_to_opensearch.py
@@ -12,7 +12,21 @@ import time
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
 HOST = os.getenv('OPENSEARCH_HOST')
 AUTH = (os.getenv('AUTH_NAME'), os.getenv('AUTH_PW'))
-print(HOST, AUTH)
+
+# 3. AWS OpenSearch 서비스 클라이언트 연결
+client = OpenSearch(
+    hosts               = [{"host":HOST, "port":443}],
+    http_auth           = AUTH,
+    http_compress       = True,
+    use_ssl             = True,
+    verify_certs        = True,
+    ssl_assert_hostname = False,
+    ssl_show_warn       = False
+)
+
+# 4. 인덱스 생성
+index_name      = 'factory-45-sensor-v1' 
+if not client.indices.exists(index=index_name):
+    client.indices.create(index=index_name)
